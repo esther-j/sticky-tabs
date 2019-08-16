eed1ea2813b56840043ab02ac884843e42f18dc6
esther-j <esther.jang0907@gmail.com>
2019-08-16 01:27:35 -0700

    Tabs move to last occurrence

--- background.js
+++ background.js
@@ -1,29 +1,77 @@
+//Whenever a tab is updated, figure out all of the previous tabs
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	if (!changeInfo.url) {
+		return;
+	}
 	console.log(changeInfo.url);
-	chrome.tabs.query({currentWindow: true}, function(tabs) {
-		tabs.forEach(function(curTab) {
-			let domain = curTab.url.match(/[^(\/\/)]*\/\/[^\/]*/);
-			var allDomains = [];
-			if (domain == null) {
-				chrome.notifications.create({
-					type: 'basic',
-					iconUrl: 'images/icon200.png',
-					requireInteraction: true,
-					title: 'Error: Unable to read URL',
-					message: 'Oops! It looks like we were unable to read that URL. \
-								Please submit a bug report - sorry about that.',
-					buttons: [
-						{title: 'Make a report'}
-					],
-					priority: 2,
-				});
+	saveTabs(tab);
+
+});
+
+
+function moveTab(tab) {
+	chrome.storage.local.get(null, function(data) {
+		//when you open a tab, you want to get its tab number
+		var domain = getDomain(tab.url);
+		var result = data[domain];
+		if (result) {
+			var prevTabIndex = result[result.length - 1];
+			chrome.tabs.move(tab.id, {'index': prevTabIndex + 1}, function() {
+				console.log("moved");
+			});
+		}
+		else {
+			console.log("did not move");
+		}
+	});
+}
+
+function saveTabs(tab) {
+	chrome.storage.local.clear();
+	chrome.tabs.query({currentWindow: true, active: false}, function(tabs) {
+		// gets all the domain indexes and writes into an object
+		var allDomains = {};
+		for (let curTab of tabs) {
+			var domain = getDomain(curTab.url);
+			if (allDomains.hasOwnProperty(domain)) {
+				allDomains[domain].push(curTab.index);
 			}
 			else {
-				domain = domain[0];
-				allDomains.push(domain);
+				allDomains[domain] = [curTab.index];
 			}
-			console.log("full tab", curTab.url);
-			console.log("this tab", domain);
+		}
+		console.log("all domains", allDomains);
+		chrome.storage.local.set(allDomains, function() {
+			if (chrome.runtime.error) {
+				console.log("a problem happened");
+			}
+			else {
+				moveTab(tab);
+			}
 		});
-	})
-});
+	});
+}
+
+function getDomain(tab) {
+	var domain = tab.match(/[^(\/\/)]*\/\/[^\/]*/);
+	//note: this is a weird case that requires bug catching
+	if (domain == null) {
+		return tab
+	}
+	return domain[0]
+}
+
+function invalidDomain() {
+	chrome.notifications.create({
+		type: 'basic',
+		iconUrl: 'images/icon200.png',
+		requireInteraction: true,
+		title: 'Error: Unable to read URL',
+		message: 'Oops! It looks like we were unable to read that URL. \
+					Please submit a bug report - sorry about that.',
+		buttons: [
+			{title: 'Make a report'}
+		],
+		priority: 2,
+	});
+}
